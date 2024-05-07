55e77e5eaf7acf84fe2342f185afeee98c50386f
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-05-06 20:36:27 -0500

    Cambiando el idioma a español para la tabla de miembros

--- resources/views/miembros/index.blade.php
+++ resources/views/miembros/index.blade.php
@@ -53,8 +53,52 @@
                         <script>
                             $(function () {
                                 $("#example1").DataTable({
+                                    "pageLength": 10,
+                                    "language": {
+                                        "emptyTable": "No hay información",
+                                        "info": "Mostrando _START_ a _END_ de _TOTAL_ Miembros",
+                                        "infoEmpty": "Mostrando 0 a 0 de 0 Miembros",
+                                        "infoFiltered": "(Filtrado de MAX total Miembros)",
+                                        "infoPostFix": "",
+                                        "thousands": ",",
+                                        "lengthMenu": "Mostrar Menu de Miembros",
+                                        "loadingRecords": "Cargando...",
+                                        "processing": "Procesando...",
+                                        "search": "Buscador:",
+                                        "zeroRecords": "Sin resultados encontrados",
+                                        "paginate": {
+                                            "first": "Primero",
+                                            "last": "Ultimo",
+                                            "next": "Siguiente",
+                                            "previous": "Anterior"
+                                        }
+                                    },
                                     "responsive": true, "lengthChange": true, "autoWidth": false,
-                                    "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+                                    buttons: [{
+                                        extend: 'collection',
+                                        text: 'Reportes',
+                                        orientation: 'landscape',
+                                        buttons: [{
+                                            text: 'Copiar',
+                                            extend: 'copy',
+                                        }, {
+                                            extend: 'pdf'
+                                        },{
+                                            extend: 'csv'
+                                        },{
+                                            extend: 'excel'
+                                        },{
+                                            text: 'Imprimir',
+                                            extend: 'print'
+                                        }
+                                        ]
+                                    },
+                                        {
+                                            extend: 'colvis',
+                                            text: 'Visor de columnas',
+                                            collectionLayout: 'fixed three-column'
+                                        }
+                                    ],
                                 }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
                             });
                         </script>
