b18828d1ae981fc8b894b12ea566c3f2e96646dc
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-05-07 21:51:43 -0500

    Creando la función store en la cual se realiza la validación de los datos que se ingresen por el formulario y se realiza el procedimiento de guardado en la base de datos

--- app/Http/Controllers/MiembroController.php
+++ app/Http/Controllers/MiembroController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Miembro;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class MiembroController extends Controller
 {
@@ -17,5 +16,38 @@ class MiembroController extends Controller
     /*TODO: redireccionando hacia la vista de creación de miembros */
     public function create(){
         return view('miembros.create');
+    }
+
+    public function store(Request $request){
+        /*$request()->all() -> pide que se rescate toda la información que se ingrese por el formulario 
+        $miembro = request()->all();
+        return response()->json($miembro);*/
+
+        /* valición de los datos que se ingresan por el formulario */
+        $request->validate([
+            'nombre_apellido' => 'required',
+            'direccion' => 'required',
+            'telefono' => 'required',
+            'fecha_nacimiento' => 'required',
+            'email' => 'required',
+            'curso' => 'required',
+        ]);
+
+        $miembro = new Miembro();
+        $miembro->nombre_apellido = $request->nombre_apellido;
+        $miembro->direccion = $request->direccion;
+        $miembro->telefono = $request->telefono;
+        $miembro->fecha_nacimiento = $request->fecha_nacimiento;
+        $miembro->genero = $request->genero;
+        $miembro->email = $request->email;
+        $miembro->estado = '1';
+        $miembro->curso = $request->curso;
+
+        //$miembro->fotografia = $request->fotografia;
+        $miembro->fotografia = $request->file('fotografia')->store('fotografias_miembros','public');
+
+        $miembro->fecha_ingreso = '2024-05-07';
+
+        $miembro->save();
     }
 }
