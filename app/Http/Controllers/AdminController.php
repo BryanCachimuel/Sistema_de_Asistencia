9aca0c1d0119cdecae5fd351d03095a768f54886
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-05-29 22:17:07 -0500

    Creando la función index donde se hace una consulta de todos los registros de cursos que estan registrados dentro de la base de datos

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Curso;
 
 class AdminController extends Controller
 {
-    //
+    public function index(){
+        $cursos = Curso::all();
+        return view('index',['cursos'=>$cursos]);
+    }
 }
