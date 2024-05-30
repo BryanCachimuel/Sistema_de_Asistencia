1c60123fd3494eb2c9634a3081a00ec66c517edf
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-05-29 22:15:30 -0500

    Añadiendo una tarjeta para poder ver la cantidad de cursos estan registrados dentro del sistema

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,36 @@
 @extends('layouts.admin')
 @section('content')
 
-    <div class="content">
+    <div class="content" style="margin: 20px">
         <h1>Página Principal</h1>
+
+        <div class="row mt-4">
+            <div class="col-lg-3">
+                <div class="small-box bg-info" style="height: 160px">
+                    <div class="inner">
+                        @php
+                            $contador_de_cursos = 0;
+                        @endphp
+                        @foreach ($cursos as $curso)
+                            @php
+                                 $contador_de_cursos += 1; 
+                            @endphp
+                        @endforeach
+
+                        <h3><?= $contador_de_cursos; ?></h3>
+                        
+                        <p>Cursos</p>
+                    </div>
+                    <div class="icon">
+                        <i class="bi bi-building-check"></i>
+                    </div>
+                    <a href="{{url('cursos')}}" class="small-box-footer" style="margin-top: 20px;">Más Información <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+
+            
+        </div>
+
     </div>
 
-@endsection
+@endsection     
