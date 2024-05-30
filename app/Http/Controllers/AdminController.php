f7a5a535bac83f12f8f62e626728d92ebb6ee011
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-05-29 22:27:59 -0500

    Añadiendo a la función index la consulta para obtener todos los registros de miembros

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Curso;
+use App\Models\Miembro;
 
 class AdminController extends Controller
 {
     public function index(){
         $cursos = Curso::all();
-        return view('index',['cursos'=>$cursos]);
+        $miembros = Miembro::all();
+        return view('index',['cursos'=>$cursos,'miembros'=>$miembros]);
     }
 }
