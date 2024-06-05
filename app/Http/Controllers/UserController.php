a19fd16a8ec3b51badbcd0a5106895dcb0d04453
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-06-04 21:37:48 -0500

    Controlador de User donde se definen las fucniones crud del proyecto, para este caso sea codificado el script del index para visualizar los datos del los usuarios registrados en una tabla

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * TODO: Creando el controlador con sus funciones: php artisan make:controller UserController -r    
+     */
+    public function index()
+    {
+        $usuarios = User::all();
+        return view('usuarios.index', ['usuarios'=>$usuarios]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
