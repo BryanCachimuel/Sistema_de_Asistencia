f6b516bc4bdc3b7fc34bca6ebaaebd541f62a2f4
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-06-05 20:51:29 -0500

    Editando el funcionamiento de la función create para que cuando se registreun usuario automaticamente se guarde la fecha de ingreso y su estadose creo de forma activa

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'fecha_ingreso' => date($format='Y-m-d'),
+            'estado' => '1'
         ]);
     }
 }
