a2a4a2b2e7090246b0bd4e54f13dfe6da30b461a
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-06-05 22:03:58 -0500

    Función validator para validar los campos de texto del formulario de registro de usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -27,7 +27,14 @@ class UserController extends Controller
         return view('usuarios.create');
     }
 
-   
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    }
 
     /**
      * Store a newly created resource in storage.
