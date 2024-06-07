75fb519e8d9a1d7f546d49bc891f8c71cd4af241
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-06-06 21:58:20 -0500

    Obteniendo todos los registros de los usuarios y colocandolos en la función index del controlador

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Curso;
 use App\Models\Miembro;
+use App\Models\User;
 
 class AdminController extends Controller
 {
     public function index(){
         $cursos = Curso::all();
         $miembros = Miembro::all();
-        return view('index',['cursos'=>$cursos,'miembros'=>$miembros]);
+        $usuarios = User::all();
+        return view('index',['cursos'=>$cursos,'miembros'=>$miembros,'usuarios'=>$usuarios]);
     }
 }
