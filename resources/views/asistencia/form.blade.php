68c0f5c888060a4c0f185e0533ba2e65be853f60
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-06-07 22:10:09 -0500

    Vistas generadas para el index, creación, vista de atributos completos de la entidad, edición y actualización de las asistencias

---
+++ resources/views/asistencia/form.blade.php
@@ -0,0 +1,19 @@
+<div class="row padding-1 p-1">
+    <div class="col-md-12">
+        
+        <div class="form-group mb-2 mb20">
+            <label for="fecha" class="form-label">{{ __('Fecha') }}</label>
+            <input type="text" name="fecha" class="form-control @error('fecha') is-invalid @enderror" value="{{ old('fecha', $asistencia?->fecha) }}" id="fecha" placeholder="Fecha">
+            {!! $errors->first('fecha', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
+        </div>
+        <div class="form-group mb-2 mb20">
+            <label for="miembro_id" class="form-label">{{ __('Miembro Id') }}</label>
+            <input type="text" name="miembro_id" class="form-control @error('miembro_id') is-invalid @enderror" value="{{ old('miembro_id', $asistencia?->miembro_id) }}" id="miembro_id" placeholder="Miembro Id">
+            {!! $errors->first('miembro_id', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
+        </div>
+
+    </div>
+    <div class="col-md-12 mt20 mt-2">
+        <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
+    </div>
+</div>
