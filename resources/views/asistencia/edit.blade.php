5eb5b4cf15160080a02c9eea68ba97bb26d38490
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-06-09 22:10:35 -0500

    Agregando el maquetado respectivo de adminlte para la vista de edición

--- resources/views/asistencia/edit.blade.php
+++ resources/views/asistencia/edit.blade.php
@@ -1,8 +1,4 @@
-@extends('layouts.app')
-
-@section('template_title')
-    {{ __('Update') }} Asistencia
-@endsection
+@extends('layouts.admin')
 
 @section('content')
     <section class="content container-fluid">
