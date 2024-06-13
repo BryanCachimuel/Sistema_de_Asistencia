23499e26e299d6eb0da75eaf477e85301ac120b6
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-06-12 22:17:03 -0500

    Modificando el aspecto de la tarjeta donde se va a poder imprimir el reporte de las asistencias

--- resources/views/asistencia/reportes.blade.php
+++ resources/views/asistencia/reportes.blade.php
@@ -19,7 +19,7 @@
                             <div class="col-md-3 col-sm-6 col-12">
                                 <div class="info-box">
                                     <span class="info-box-icon bg-info">
-                                        <a href="{{url('asistencia/reportes_pdf')}}">
+                                        <a href="{{url('asistencias/reportes_pdf')}}">
                                             <i class="bi bi-printer"></i>
                                         </a>
                                     </span>
