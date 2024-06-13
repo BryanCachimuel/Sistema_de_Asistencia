4e073878bbcdfbbd9e063071a440510e152f7369
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-06-12 22:18:11 -0500

    Vista para imprimir la información de los reportes

---
+++ resources/views/asistencia/pdf.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Reporte de Asistencia</title>
+</head>
+
+<body>
+    <h1>Reporte de Asistencia</h1>
+
+    <table border="1">
+        <thead class="thead">
+            <tr>
+                <th>No</th>
+                <th>Fecha Asistencia</th>
+                <th>Nombres de los Miembros</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($asistencias as $asistencia)
+                <tr>
+                    <td>{{ ++$i }}</td>
+                    <td>{{ $asistencia->fecha }}</td>
+                    <td>{{ $asistencia->miembro->nombre_apellido }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+
+</html>
