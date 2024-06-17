89751a851c5c202ee08a8d0880777ce21908e9f3
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-06-16 21:16:22 -0500

    Vista de la impresión de los reportes por fechas específicas

---
+++ resources/views/asistencia/pdfFechas.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Reporte de Asistencia por Fechas</title>
+</head>
+
+<body>
+    <h1>Reporte de Asistencia por Fechas</h1>
+
+    <table border="1">
+        <thead class="thead">
+            <tr>
+                <th>No</th>
+                <th>Fecha Asistencia</th>
+                <th>Nombres de los Miembros</th>
+            </tr>
+        </thead>
+        <tbody>
+            @php $contador_asistencia = 1;  @endphp
+            @foreach ($asistencias as $asistencia)
+                <tr>
+                    <td><?= $contador_asistencia++; ?></td>
+                    <td>{{ $asistencia->fecha }}</td>
+                    <td>{{ $asistencia->miembro->nombre_apellido }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+
+</html>
