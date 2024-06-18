4f0336092c3fff4965367a121a8638b1ddcd9faa
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-06-17 20:54:54 -0500

    Vista donde se encuentra la tarjeta para la impresión de reportes de los miembros

---
+++ resources/views/miembros/reportes.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container-fluid">
+        <h1>Reportes de Miembros</h1>
+      
+        <div class="row">
+            <div class="col-sm-12 mt-2">
+                <div class="card card-outline card-primary">
+                    <div class="card-header">
+                        <div style="display: flex; justify-content: space-between; align-items: center;">
+                            <h3 class="card-title"><strong>Reporte de Miembros</strong></h3>
+                        </div>
+                    </div>
+
+                    <div class="card-body bg-white">
+                       
+                        <div class="row">
+                            <div class="col-md-3 col-sm-6 col-12">
+                                <div class="info-box"  style="height:92px">
+                                    <span class="info-box-icon bg-warning">
+                                        <a href="{{url('miembros/reportes_pdf')}}" target="_blank">
+                                            <i class="bi bi-printer"></i>
+                                        </a>
+                                    </span>
+                                    <div class="info-box-content">
+                                        <span class="info-box-text">Imprimir Reporte</span>
+                                        <span class="info-box-number">Miembros</span>
+                                    </div>
+                                </div>
+                            </div>
+
+                     
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
